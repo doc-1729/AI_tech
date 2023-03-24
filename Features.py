55e7d750e1ad0d58c8cc46ee8d270743aae8a4f3
Alex-Yola <doc-1729@yandex.com>
2023-03-24 09:46:35 +0300

    Незначительные улучшения читаемости кода, удалены лишние import

--- Features.py
+++ Features.py
@@ -2,7 +2,6 @@
 Генерация и визуализация датасета (2 класса, 2-D пространство признаков)
 """
 
-import math
 import random as rnd
 import pandas as pd
 import numpy as np
@@ -13,10 +12,7 @@ rnd.seed(1000)  # Инициализация генератора псевдос
 N = 13  # Число объектов в каждом классе
 
 # Генерация случайных значений признаков объектов класса y=2 для обучающей выборки
-m_x1_2 = 6
-sigma_x1_2 = 1.5
-m_x2_2 = 0
-sigma_x2_2 = 0.7
+m_x1_2 = 6; sigma_x1_2 = 1.5; m_x2_2 = 0; sigma_x2_2 = 0.7
 x1_2 = np.asarray([rnd.gauss(m_x1_2, sigma_x1_2) for i in range(N)])
 x2_2 = np.asarray([rnd.gauss(m_x2_2, sigma_x2_2) for i in range(N)])
 m1_2, m2_2 = np.mean(x1_2), np.mean(x2_2)  # центр тяжести
@@ -28,10 +24,7 @@ plt.scatter(x1_2, x2_2, marker='D', color='green')  # y=2
 plt.scatter(m1_2, m2_2, marker='D', color='limegreen', s=100)  # центр тяжести
 
 # Генерация случайных значений признаков объектов класса y=1 для обучающей выборки
-m_x1_1 = 2;
-sigma_x1_1 = 0.6;
-m_x2_1 = 5;
-sigma_x2_1 = 1
+m_x1_1 = 2; sigma_x1_1 = 0.6; m_x2_1 = 5; sigma_x2_1 = 1
 x1_1 = np.asarray([rnd.gauss(m_x1_1, sigma_x1_1) for i in range(N)])
 x2_1 = np.asarray([rnd.gauss(m_x2_1, sigma_x2_1) for i in range(N)])
 m1_1, m2_1 = np.mean(x1_1), np.mean(x2_1)  # центр тяжести
@@ -41,8 +34,7 @@ plt.scatter(x1_1, x2_1, marker='s', color='dodgerblue')  # y=1
 plt.scatter(m1_1, m2_1, marker='D', color='blue', s=100)  # центр тяжести
 
 # задание признаков наблюдаемого объекта
-x1_nabl = 5;
-x2_nabl = 3
+x1_nabl = 5; x2_nabl = 3
 plt.scatter([x1_nabl], [x2_nabl], marker='o', color='red', s=100)  # наблюдаемый образ
 
 # Оформление графика
@@ -65,17 +57,17 @@ plt.show()
 """
 Преобразование обучающей выборки к формату DataFrame
 """
-#
+
 data = np.ones((N, 4))
 df1 = pd.DataFrame(data, columns=['x1', 'x2', 'class', 'distanse'], index=range(N))
-df1['x1'] = x1_1;
-df1['x2'] = x2_1;
-df1['class'] = 'y=1';
+df1['x1'] = x1_1
+df1['x2'] = x2_1
+df1['class'] = 'y=1'
 df1['distanse'] = float('inf')
 df2 = pd.DataFrame(data, columns=['x1', 'x2', 'class', 'distanse'], index=range(N, 2 * N))
-df2['x1'] = x1_2;
-df2['x2'] = x2_2;
-df2['class'] = 'y=2';
+df2['x1'] = x1_2
+df2['x2'] = x2_2
+df2['class'] = 'y=2'
 df2['distanse'] = float('inf')
 df = pd.concat([df1, df2], axis=0)
 print(df)
@@ -85,16 +77,15 @@ print(df)
 
 
 """
-Визуализация набора данных из файла *.csv
+Визуализация набора данных из файла *.csv 
 """
-# import pandas as pd
+import pandas as pd
 import seaborn as sns
 
 # Чтение набора данных из файла в DataFrame
 df = pd.read_csv('x1_x2_class_distanse_2x13.csv')
 
 # Визуализаци DataFrame средствами продвинутой графики seaborn
-fig2 = plt.figure()
 mark = {'y=1': "s", "y=2": "D"}
 sns.scatterplot(data=df, x="x1", y="x2", hue="class", style="class", s=100, markers=mark)
 plt.show()
@@ -103,7 +94,8 @@ plt.show()
 Визуализация готового набора данных о цветке Ирис 
 из учебного репозитория
 https://archive.ics.uci.edu/ml/datasets/Iris
-з класса, 4 признака"""
+з класса, 4 признака
+"""
 
 # Чтение набора данных из файла
 iris_data = pd.read_csv('iris.csv')
@@ -124,48 +116,42 @@ ax4 = fig.add_subplot(2, 3, 4)
 ax5 = fig.add_subplot(2, 3, 5)
 ax6 = fig.add_subplot(2, 3, 6)
 
-a = "x1";
-b = "x2"
+a = "x1"; b = "x2"
 ax1.scatter(df_y1[a], df_y1[b])
 ax1.scatter(df_y2[a], df_y2[b])
 ax1.scatter(df_y3[a], df_y3[b])
 ax1.set_xlabel(a)
 ax1.set_ylabel(b)
 
-a = "x1";
-b = "x3"
+a = "x1"; b = "x3"
 ax2.scatter(df_y1[a], df_y1[b])
 ax2.scatter(df_y2[a], df_y2[b])
 ax2.scatter(df_y3[a], df_y3[b])
 ax2.set_xlabel(a)
 ax2.set_ylabel(b)
 
-a = "x1";
-b = "x4"
+a = "x1"; b = "x4"
 ax3.scatter(df_y1[a], df_y1[b])
 ax3.scatter(df_y2[a], df_y2[b])
 ax3.scatter(df_y3[a], df_y3[b])
 ax3.set_xlabel(a)
 ax3.set_ylabel(b)
 
-a = "x2";
-b = "x3"
+a = "x2"; b = "x3"
 ax4.scatter(df_y1[a], df_y1[b])
 ax4.scatter(df_y2[a], df_y2[b])
 ax4.scatter(df_y3[a], df_y3[b])
 ax4.set_xlabel(a)
 ax4.set_ylabel(b)
 
-a = "x2";
-b = "x4"
+a = "x2"; b = "x4"
 ax5.scatter(df_y1[a], df_y1[b])
 ax5.scatter(df_y2[a], df_y2[b])
 ax5.scatter(df_y3[a], df_y3[b])
 ax5.set_xlabel(a)
 ax5.set_ylabel(b)
 
-a = "x3";
-b = "x4"
+a = "x3"; b = "x4"
 ax6.scatter(df_y1[a], df_y1[b])
 ax6.scatter(df_y2[a], df_y2[b])
 ax6.scatter(df_y3[a], df_y3[b])
@@ -177,7 +163,7 @@ plt.show()
 """
 Проекции 4D признакового пространства в 3D
 """
-from mpl_toolkits import mplot3d
+# from mpl_toolkits import mplot3d
 
 # Чтение набора данных из файла
 iris_data = pd.read_csv('iris.csv')
@@ -191,9 +177,7 @@ df_y3 = iris_data[iris_data["class"] == "Iris-virginica"]
 fig = plt.figure(figsize=(16, 9))
 ax = plt.axes(projection='3d')
 
-a = "x1";
-b = "x3";
-c = "x2"
+a = "x1"; b = "x3"; c = "x2"
 ax.scatter3D(df_y1[a], df_y1[b], df_y1[c], s=100)
 ax.scatter3D(df_y2[a], df_y2[b], df_y1[c], s=100)
 ax.scatter3D(df_y3[a], df_y3[b], df_y1[c], s=100)
@@ -204,9 +188,7 @@ ax.set_zlabel(c, fontweight='bold')
 fig = plt.figure(figsize=(16, 9))
 ax1 = plt.axes(projection='3d')
 
-a = "x4";
-b = "x3";
-c = "x1"
+a = "x4"; b = "x3"; c = "x1"
 ax1.scatter3D(df_y1[a], df_y1[b], df_y1[c], s=100)
 ax1.scatter3D(df_y2[a], df_y2[b], df_y1[c], s=100)
 ax1.scatter3D(df_y3[a], df_y3[b], df_y1[c], s=100)
@@ -217,9 +199,7 @@ ax1.set_zlabel(c, fontweight='bold')
 fig = plt.figure(figsize=(16, 9))
 ax2 = plt.axes(projection='3d')
 
-a = "x4";
-b = "x1";
-c = "x2"
+a = "x4"; b = "x1"; c = "x2"
 ax2.scatter3D(df_y1[a], df_y1[b], df_y1[c], s=100)
 ax2.scatter3D(df_y2[a], df_y2[b], df_y1[c], s=100)
 ax2.scatter3D(df_y3[a], df_y3[b], df_y1[c], s=100)
@@ -230,9 +210,7 @@ ax2.set_zlabel(c, fontweight='bold')
 fig = plt.figure(figsize=(16, 9))
 ax3 = plt.axes(projection='3d')
 
-a = "x4";
-b = "x2";
-c = "x3"
+a = "x4"; b = "x2"; c = "x3"
 ax3.scatter3D(df_y1[a], df_y1[b], df_y1[c], s=100)
 ax3.scatter3D(df_y2[a], df_y2[b], df_y1[c], s=100)
 ax3.scatter3D(df_y3[a], df_y3[b], df_y1[c], s=100)
@@ -262,7 +240,6 @@ plt.show()
 с помощью лепестковой диаграммы
 """
 import pandas as pd
-import seaborn as sns
 import numpy as np
 import matplotlib.pyplot as plt
 
